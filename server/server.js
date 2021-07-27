b84834856a10f111cbe23de43690456f8670c209
Dane Petersen <dane.m.petersen@gmail.com>
2021-07-27 16:36:35 -0500

    added gitignore

--- server/server.js
+++ server/server.js
@@ -6,8 +6,14 @@ const express = require('express');
 
 const app = express();
 
+
+// Tell express where to find all of our public files ( aka "client-side" files ) 
+// Also called static assets
+app.use(express.static('./server/public'));
+
+
 // Listen for requests
 app.listen(5000, function() {
   // Kind of like our onReady function
-  console.log('App is runnin on localhost:5000');
+  console.log('App is running on localhost:5000');
 });
